cd2d2063e642d96b512349118ec3d1aa8c9c91ea
bigchiano <bigchiano@protonmail.com>
2026-02-17 19:26:48 +0300

    chore: disable Base Mainnet syncing temporarily

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,10 +4,10 @@ import { BORROW_LOGIC_ABI } from "./abis/borrow-logic";
 
 export default createConfig({
   chains: {
-    base: {
-      id: 8453,
-      rpc: process.env.PONDER_RPC_URL_8453,
-    },
+    // base: {
+    //   id: 8453,
+    //   rpc: process.env.PONDER_RPC_URL_8453,
+    // },
     baseSepolia: {
       id: 84532,
       rpc: process.env.PONDER_RPC_URL_84532,
@@ -17,10 +17,10 @@ export default createConfig({
     FundingBook: {
       abi: FUNDING_BOOK_ABI,
       chain: {
-        base: {
-          address: process.env.FUNDING_BOOK_ADDRESS as `0x${string}`,
-          startBlock: Number(process.env.FUNDING_BOOK_START_BLOCK),
-        },
+        // base: {
+        //   address: process.env.FUNDING_BOOK_ADDRESS as `0x${string}`,
+        //   startBlock: Number(process.env.FUNDING_BOOK_START_BLOCK),
+        // },
         baseSepolia: {
           address: process.env.FUNDING_BOOK_ADDRESS_TESTNET as `0x${string}`,
           startBlock: Number(process.env.FUNDING_BOOK_START_BLOCK_TESTNET),
@@ -30,10 +30,10 @@ export default createConfig({
     BorrowingLogic: {
       abi: BORROW_LOGIC_ABI,
       chain: {
-        base: {
-          address: process.env.BORROW_LOGIC_ADDRESS as `0x${string}`,
-          startBlock: Number(process.env.BORROW_LOGIC_START_BLOCK),
-        },
+        // base: {
+        //   address: process.env.BORROW_LOGIC_ADDRESS as `0x${string}`,
+        //   startBlock: Number(process.env.BORROW_LOGIC_START_BLOCK),
+        // },
         baseSepolia: {
           address: process.env.BORROW_LOGIC_ADDRESS_TESTNET as `0x${string}`,
           startBlock: Number(process.env.BORROW_LOGIC_START_BLOCK_TESTNET),
